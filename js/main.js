bf2c772d37f49a13d338071fcc9930ea206a00f0
Karen Bu <ktianbu@gmail.com>
2025-04-08 04:11:16 +0000

    issue 1: added 'submit' event listener to the form, prevented the page from refreshing when the form is submitted, stored the form's input values in a new object

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 'use strict';
+// Photo Preview from Pasting URL //
 const $photoURL = document.querySelector('#photo-url');
 if (!$photoURL) throw new Error('$photoURL does not exist!');
 const $image = document.querySelector('img');
 if (!$image) throw new Error('$image does not exist!');
-// Reference image URLs to test code
-// https://64.media.tumblr.com/704b217831d890fee3e84c5f28646889/dfeae80de15b3de7-08/s1280x1920/78f8d964fcff80916d40ea5e6121afbc5562f283.jpg
-// https://64.media.tumblr.com/1b54e834bda85fc26bc6a018e5f9b444/a3ba198d6b60f48d-cd/s1280x1920/4b031174a9cb50953918afe5ca37e1143ff5ee80.jpg
 $photoURL.addEventListener('input', (event) => {
   const inputURL = event.target;
   $image.src = inputURL.value;
 });
-const $entryForm = document.querySelector('#entry-form').elements;
+const $entryForm = document.querySelector('form');
 if (!$entryForm) throw new Error('$entryForm does not exist!');
-// const $entryFormInputs = $entryForm.elements;
-// console.log($entryForm.elements);
-// $entryForm.addEventListener('submit', (event: Event) => {
-//   event.preventDefault();
-//   console.log($entryForm.elements);
-// });
-// const formSubmit = event.target;
-// console.log('event target:', event.target);
-// console.log('button clicked!', formSubmit);
-// const formInput = event.input.value;
+const $entryFormInputs = $entryForm.elements;
+$entryForm.addEventListener('submit', (event) => {
+  // prevent the page from refreshing
+  event.preventDefault();
+  // stores the form's input values in a new object
+  const entryTitle = $entryFormInputs.title.value;
+  const entryPhotoURL = $entryFormInputs.photoURL.value;
+  const entryNotes = $entryFormInputs.notes.value;
+  const entryObject = { entryTitle, entryPhotoURL, entryNotes };
+  // assigns an entryID property to the new object, taken from the nextEntryID property of the data model
+});
+console.log($entryForm);
+console.log($entryFormInputs);
--- ts/main.ts
+++ ts/main.ts
@@ -1,32 +1,50 @@
+// Photo Preview from Pasting URL //
+
 const $photoURL = document.querySelector('#photo-url');
 if (!$photoURL) throw new Error('$photoURL does not exist!');
 
 const $image = document.querySelector('img') as HTMLImageElement;
 if (!$image) throw new Error('$image does not exist!');
 
-// Reference image URLs to test code
-// https://64.media.tumblr.com/704b217831d890fee3e84c5f28646889/dfeae80de15b3de7-08/s1280x1920/78f8d964fcff80916d40ea5e6121afbc5562f283.jpg
-// https://64.media.tumblr.com/1b54e834bda85fc26bc6a018e5f9b444/a3ba198d6b60f48d-cd/s1280x1920/4b031174a9cb50953918afe5ca37e1143ff5ee80.jpg
-
 $photoURL.addEventListener('input', (event: Event) => {
   const inputURL = event.target as HTMLInputElement;
   $image.src = inputURL.value;
 });
 
-// const $entryForm = document.querySelector('#entry-form')
-//   .elements as HTMLFormControlsCollection;
-// if (!$entryForm) throw new Error('$entryForm does not exist!');
+// Ref URL: https://64.media.tumblr.com/1b54e834bda85fc26bc6a018e5f9b444/a3ba198d6b60f48d-cd/s1280x1920/4b031174a9cb50953918afe5ca37e1143ff5ee80.jpg
 
-// const $entryFormInputs = $entryForm.elements;
-// console.log($entryForm.elements);
+// Submitting the Form
 
-// $entryForm.addEventListener('submit', (event: Event) => {
-//   event.preventDefault();
-//   console.log($entryForm.elements);
-// });
+interface FormElements extends HTMLFormControlsCollection {
+  title: HTMLInputElement;
+  photoURL: HTMLInputElement;
+  notes: HTMLTextAreaElement;
+}
 
-// const formSubmit = event.target;
+interface EntryObject {
+  entryTitle: string;
+  entryPhotoURL: string;
+  entryNotes: string;
+}
 
-// console.log('event target:', event.target);
-// console.log('button clicked!', formSubmit);
-// const formInput = event.input.value;
+const $entryForm = document.querySelector('form') as HTMLFormElement;
+if (!$entryForm) throw new Error('$entryForm does not exist!');
+
+const $entryFormInputs = $entryForm.elements as FormElements;
+
+$entryForm.addEventListener('submit', (event: Event) => {
+  // prevent the page from refreshing
+  event.preventDefault();
+
+  // stores the form's input values in a new object
+  const entryTitle = $entryFormInputs.title.value;
+  const entryPhotoURL = $entryFormInputs.photoURL.value;
+  const entryNotes = $entryFormInputs.notes.value;
+  const entryObject: EntryObject = { entryTitle, entryPhotoURL, entryNotes };
+  console.log(entryObject);
+
+  // assigns an entryID property to the new object, taken from the nextEntryID property of the data model
+});
+
+console.log($entryForm);
+console.log($entryFormInputs);
