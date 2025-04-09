d3ef805d2daf1b5bf6f910c5bb2a224049ec85e1
Karen Bu <ktianbu@gmail.com>
2025-04-09 02:23:50 +0000

    issue 1: cleaned up code

--- ts/main.ts
+++ ts/main.ts
@@ -11,9 +11,6 @@ $photoURL.addEventListener('input', (event: Event) => {
   $image.src = inputURL.value;
 });
 
-// Ref URL 1: https://64.media.tumblr.com/1b54e834bda85fc26bc6a018e5f9b444/a3ba198d6b60f48d-cd/s1280x1920/4b031174a9cb50953918afe5ca37e1143ff5ee80.jpg
-// Ref URL 2: https://64.media.tumblr.com/a676bf6547d045021cd4f0d59a5a6bcf/69b2f4deb5e1518b-c5/s1280x1920/2ed51076367f818726f73ff58c62fe5943a89aaf.jpg
-
 // SUBMITTING THE FORM
 
 interface FormElements extends HTMLFormControlsCollection {
