a97bbba2fccde3fbeffea41c902f7e9387a96c96
Karen Bu <ktianbu@gmail.com>
2025-04-13 05:15:31 +0000

    issue 2: finished work

--- ts/main.ts
+++ ts/main.ts
@@ -27,6 +27,9 @@ if (!$entryForm) throw new Error('$entryForm does not exist!');
 const $entryFormInputs = $entryForm.elements as FormElements;
 
 // test url: https://64.media.tumblr.com/1b54e834bda85fc26bc6a018e5f9b444/a3ba198d6b60f48d-cd/s1280x1920/4b031174a9cb50953918afe5ca37e1143ff5ee80.jpg
+// test url 2: https://64.media.tumblr.com/a676bf6547d045021cd4f0d59a5a6bcf/69b2f4deb5e1518b-c5/s1280x1920/2ed51076367f818726f73ff58c62fe5943a89aaf.jpg
+// test url 3: https://64.media.tumblr.com/81d06a9d4b7488ba4680367802e24e4d/dd16d4a1f06633be-10/s1280x1920/cda28cd49a068f497661b96065a359d48e93d308.jpg
+// test url 4: https://64.media.tumblr.com/704b217831d890fee3e84c5f28646889/dfeae80de15b3de7-08/s1280x1920/78f8d964fcff80916d40ea5e6121afbc5562f283.jpg
 
 $entryForm.addEventListener('submit', (event: Event) => {
   // prevents the page from refreshing
@@ -39,7 +42,6 @@ $entryForm.addEventListener('submit', (event: Event) => {
     entryNotes: $entryFormInputs.notes.value,
     entryID: data.nextEntryId,
   };
-  console.log(newEntry);
 
   // increments the nextEntryId property of the data model
   data.nextEntryId++;
@@ -55,6 +57,12 @@ $entryForm.addEventListener('submit', (event: Event) => {
 
   // writes the modified data model to localStorage
   writeEntry();
+
+  // render a DOM tree for the newly submitted entry object
+  renderEntry(newEntry);
+
+  // shows the entries view
+  viewSwap('entries');
 });
 
 // ---------------- ISSUE 2 ---------------- //
@@ -102,6 +110,7 @@ document.addEventListener('DOMContentLoaded', (): void => {
     $newEntryColumn2?.appendChild($newEntryH1);
     $newEntryColumn2?.appendChild($newEntryNotes);
   }
+  viewSwap('entries');
 });
 
 // create a function to toggle no entries text to show or hide when the function is called
@@ -127,12 +136,10 @@ function viewSwap(viewName: any): void {
     $entryForm.className = 'view entryform hidden';
     $entryList.className = 'view entrylist';
     data.view = 'entries';
-    console.log(data);
   } else if (viewName === 'entry-form') {
     $entryForm.className = 'view entryform';
     $entryList.className = 'view entrylist hidden';
     data.view = 'entry-form';
-    console.log(data);
   }
 }
 
@@ -140,18 +147,18 @@ function viewSwap(viewName: any): void {
 
 // for the entry list view
 const $entriesAnchorEntries = document.querySelector('#entries-anchor-entries');
-$entriesAnchorEntries?.addEventListener('click', (event: Event): void => {
+$entriesAnchorEntries?.addEventListener('click', (): void => {
   viewSwap('entries');
 });
 
 // for the entry form view
 const $entriesAnchorForm = document.querySelector('#entries-anchor-entryform');
-$entriesAnchorForm?.addEventListener('click', (event: Event): void => {
+$entriesAnchorForm?.addEventListener('click', (): void => {
   viewSwap('entries');
 });
 
 // add an anchor to the entries view to show the entry-form via "new" button
 const $newEntryButton = document.querySelector('#new-button');
-$newEntryButton?.addEventListener('click', (event: Event): void => {
+$newEntryButton?.addEventListener('click', (): void => {
   viewSwap('entry-form');
 });
