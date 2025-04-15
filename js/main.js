9dfcb16b38dc44595e06590ea6baece1355d43fa
Karen Bu <ktianbu@gmail.com>
2025-04-15 06:53:29 +0000

    issue 3: updated renderEntry function to add pencil icon to the title of each entry and assign data-entry-id attribute to the li of the entry

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 'use strict';
-// ---------------- ISSUE 1 ---------------- //
 // PHOTO PREVIEW FROM PASTING URL //
 const $photoURL = document.querySelector('#photo-url');
 if (!$photoURL) throw new Error('$photoURL does not exist!');
@@ -12,7 +11,7 @@ $photoURL.addEventListener('input', (event) => {
 const $entryForm = document.querySelector('form');
 if (!$entryForm) throw new Error('$entryForm does not exist!');
 const $entryFormInputs = $entryForm.elements;
-// test url: https://64.media.tumblr.com/1b54e834bda85fc26bc6a018e5f9b444/a3ba198d6b60f48d-cd/s1280x1920/4b031174a9cb50953918afe5ca37e1143ff5ee80.jpg
+// test url 1: https://64.media.tumblr.com/1b54e834bda85fc26bc6a018e5f9b444/a3ba198d6b60f48d-cd/s1280x1920/4b031174a9cb50953918afe5ca37e1143ff5ee80.jpg
 // test url 2: https://64.media.tumblr.com/a676bf6547d045021cd4f0d59a5a6bcf/69b2f4deb5e1518b-c5/s1280x1920/2ed51076367f818726f73ff58c62fe5943a89aaf.jpg
 // test url 3: https://64.media.tumblr.com/81d06a9d4b7488ba4680367802e24e4d/dd16d4a1f06633be-10/s1280x1920/cda28cd49a068f497661b96065a359d48e93d308.jpg
 // test url 4: https://64.media.tumblr.com/704b217831d890fee3e84c5f28646889/dfeae80de15b3de7-08/s1280x1920/78f8d964fcff80916d40ea5e6121afbc5562f283.jpg
@@ -37,55 +36,79 @@ $entryForm.addEventListener('submit', (event) => {
   // writes the modified data model to localStorage
   writeEntry();
   // render a DOM tree for the newly submitted entry object
-  renderEntry(newEntry);
+  const $ul = document.querySelector('ul');
+  if (!$ul) throw new Error('$ul does not exist!');
+  $ul.prepend(renderEntry(newEntry));
   // shows the entries view
   viewSwap('entries');
 });
-// ---------------- ISSUE 2 ---------------- //
 // VIEWING ENTRIES
 // generating and returning a DOM tree for a single entry in the UL
 function renderEntry(entry) {
-  return data.entries[entry];
+  // creating new entry
+  const $newEntry = document.createElement('li');
+  $newEntry.setAttribute('class', 'journal-entry');
+  $newEntry.setAttribute('data-entry-id', String(entry.entryID));
+  const $newEntryRow = document.createElement('div');
+  $newEntryRow.setAttribute('class', 'row entry');
+  const $newEntryColumnPhoto = document.createElement('div');
+  $newEntryColumnPhoto.setAttribute('class', 'column-half entry-img');
+  const $newEntryPhoto = document.createElement('img');
+  $newEntryPhoto.setAttribute('src', entry.entryPhotoURL);
+  const $newEntryColumnText = document.createElement('div');
+  $newEntryColumnText.setAttribute('class', 'column-half');
+  const $newEntryRowTitleIcon = document.createElement('div');
+  $newEntryRowTitleIcon.setAttribute('class', 'row entry');
+  const $newEntryColumnTitle = document.createElement('div');
+  $newEntryColumnTitle.setAttribute('class', 'column-half');
+  const $newEntryColumnIcon = document.createElement('div');
+  $newEntryColumnIcon.setAttribute('class', 'column-full');
+  const $newEntryHeading = document.createElement('h2');
+  $newEntryHeading.textContent = entry.entryTitle;
+  $newEntryHeading.setAttribute('class', 'column-half');
+  const $editEntryImg = document.createElement('i');
+  $editEntryImg.setAttribute('class', 'fa-solid fa-pen-to-square edit-img');
+  const $newEntryNotes = document.createElement('p');
+  $newEntryNotes.textContent = entry.entryNotes;
+  // appending new entries to DOM
+  // entry row
+  $newEntry?.appendChild($newEntryRow);
+  // column for first half of entry
+  $newEntryRow?.appendChild($newEntryColumnPhoto);
+  $newEntryColumnPhoto?.appendChild($newEntryPhoto);
+  // column for second half of entry
+  $newEntryRow?.appendChild($newEntryColumnText);
+  // // column to hold title + icon
+  $newEntryColumnText?.appendChild($newEntryRowTitleIcon);
+  $newEntryRowTitleIcon?.appendChild($newEntryColumnTitle);
+  $newEntryColumnTitle?.appendChild($newEntryHeading);
+  $newEntryRowTitleIcon?.appendChild($newEntryColumnIcon);
+  $newEntryColumnIcon?.appendChild($editEntryImg);
+  // row to hold notes
+  $newEntryColumnText?.appendChild($newEntryNotes);
+  return $newEntry;
 }
-// event listener for DOMContentLoaded event
 document.addEventListener('DOMContentLoaded', () => {
+  const $ul = document.querySelector('ul');
   for (let i = 0; i < data.entries.length; i++) {
-    // creating new entry
-    const $ul = document.querySelector('ul');
-    const $newEntry = document.createElement('li');
-    $newEntry.setAttribute('class', 'journal-entry');
-    const $newEntryRow = document.createElement('div');
-    $newEntryRow.setAttribute('class', 'row');
-    const $newEntryColumn1 = document.createElement('div');
-    $newEntryColumn1.setAttribute('class', 'column-half entry-img');
-    const $newEntryPhoto = document.createElement('img');
-    $newEntryPhoto.setAttribute('src', renderEntry(i).entryPhotoURL);
-    const $newEntryColumn2 = document.createElement('div');
-    $newEntryColumn2.setAttribute('class', 'column-half entry-text');
-    const $newEntryH1 = document.createElement('h1');
-    $newEntryH1.textContent = renderEntry(i).entryTitle;
-    const $newEntryNotes = document.createElement('p');
-    $newEntryNotes.textContent = renderEntry(i).entryNotes;
-    // appending new entries to DOM
-    $ul?.appendChild($newEntry);
-    $newEntry?.appendChild($newEntryRow);
-    $newEntryRow?.appendChild($newEntryColumn1);
-    $newEntryColumn1?.appendChild($newEntryPhoto);
-    $newEntryRow?.appendChild($newEntryColumn2);
-    $newEntryColumn2?.appendChild($newEntryH1);
-    $newEntryColumn2?.appendChild($newEntryNotes);
+    $ul?.appendChild(renderEntry(data.entries[i]));
   }
-  viewSwap('entries');
+  viewSwap(data.view);
 });
 // create a function to toggle no entries text to show or hide when the function is called
 function toggleNoEntries() {
-  if (data.nextEntryId === 1) {
-    const $noEntryDiv = document.querySelector('.no-entries');
-    if (!$noEntryDiv) throw new Error('$noEntryDiv does not exist!');
+  const $noEntryDiv = document.querySelector('.no-entries');
+  if (!$noEntryDiv) throw new Error('$noEntryDiv does not exist!');
+  if (data.view === 'entry-form') {
+    $noEntryDiv.className = 'column-full no-entries hidden';
+  } else if (data.view === 'entries' && data.nextEntryId <= 1) {
     $noEntryDiv.className = 'column-full no-entries';
-  } else readEntry();
+  } else if (data.view === 'entries' && data.nextEntryId > 1) {
+    $noEntryDiv.className = 'column-full no-entries hidden';
+  } else {
+    readEntry();
+  }
 }
-toggleNoEntries();
 // create function to swap views between entries or entry-form
 function viewSwap(viewName) {
   const $entryForm = document.querySelector('.view.entryform');
@@ -101,16 +124,11 @@ function viewSwap(viewName) {
     $entryList.className = 'view entrylist hidden';
     data.view = 'entry-form';
   }
+  toggleNoEntries();
 }
 // add anchor to navbar to show 'entries' view
-// for the entry list view
-const $entriesAnchorEntries = document.querySelector('#entries-anchor-entries');
+const $entriesAnchorEntries = document.querySelector('.entries-anchor');
 $entriesAnchorEntries?.addEventListener('click', () => {
-  viewSwap('entries');
-});
-// for the entry form view
-const $entriesAnchorForm = document.querySelector('#entries-anchor-entryform');
-$entriesAnchorForm?.addEventListener('click', () => {
   viewSwap('entries');
 });
 // add an anchor to the entries view to show the entry-form via "new" button
